e63495e83b657e09e3776db7d6adc67b39bb2fe6
LucasBalbueno <balbuenolucas04@gmail.com>
2023-12-22 12:11:02 -0300

    Criando animação mockup iphone

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
 import './animations/buttonProjects.js'
 import textLoad from './animations/IamText.js'
 import './animations/observer.js'
+import './animations/vanilla-tilt.js'
 
 textLoad()
 setInterval(textLoad, 12000);
+
+VanillaTilt.init(document.querySelectorAll("#div3dEffect"), {
+    max: 20,
+    speed: 300,
+});
